4de384db28be4b488474822ac3931d798297185c
Ken Harmon <harmkenn@gmail.com>
2024-10-22 11:54:32 +0000

    Update 09

--- apps/BuySellHold2.py
+++ apps/BuySellHold2.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 
 # Set up the Streamlit interface
-st.title("OHLC Chart")
+st.title("Closing Price Chart")
 
 
 # Set the ticker symbol
@@ -13,22 +13,24 @@ ticker = st.text_input("Enter ticker symbol", value="AAPL")
 
 
 # Set the start and end dates
-start_date = st.date_input("Select start date", value=dt.date(2020, 1, 1), min_value=dt.date(2010, 1, 1), max_value=dt.date.today()) 
+start_date = st.date_input("Select start date", value=dt.date(2020, 1, 1), min_value=dt.date(2010, 1, 1), max_value=dt.date.today())
 end_date = st.date_input("Select end date", value=dt.date.today(), min_value=dt.date(2010, 1, 1), max_value=dt.date.today())
+
+
 # Fetch historical data for the selected ticker and date range
 data = yf.download(ticker, start=start_date, end=end_date, interval='1d')
 
 
-# Create the OHLC chart
+# Create the closing price chart
 fig = go.Figure()
 
 
-# Add OHLC data
-fig.add_trace(go.Ohlc(x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name='OHLC'))
+# Add closing price data
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Closing Price'))
 
 
 # Update layout
-fig.update_layout(title=f'OHLC Chart for {ticker}',xaxis_title='Date', yaxis_title='Price',xaxis_rangeslider_visible=True)
+fig.update_layout(title=f'Closing Price Chart for {ticker}',xaxis_title='Date',yaxis_title='Price')
 
 
 # Streamlit app layout
