8b02239a023071265970d50b2e40c728c6e91b95
Ken Harmon <harmkenn@gmail.com>
2024-10-30 18:38:25 +0100

    Update 18

--- apps/BuySellHold2.py
+++ apps/BuySellHold2.py
@@ -1,25 +1,16 @@
 import streamlit as st
 import yfinance as yf
-import seaborn as sns
-import matplotlib.pyplot as plt
+import mplfinance as mpf
 
-st.title("Stock OHLC Graph")
+def plot_ohlc(tickerSymbol):
+    tickerData = yf.Ticker(tickerSymbol)
+    tickerDf = tickerData.history(period='1y')
 
-stock_symbol = st.text_input("Enter stock symbol (e.g. AAPL, GOOG, etc.)")
-
-if stock_symbol:
-    data = yf.download(stock_symbol, period="5y")
-
-    fig, ax = plt.subplots(figsize=(12, 6))
-
-    sns.lineplot(x=data.index, y=data['Open'], ax=ax, label='Open')
-    sns.lineplot(x=data.index, y=data['High'], ax=ax, label='High')
-    sns.lineplot(x=data.index, y=data['Low'], ax=ax, label='Low')
-    sns.lineplot(x=data.index, y=data['Close'], ax=ax, label='Close')
-
-    ax.set_title(f"{stock_symbol} OHLC Graph")
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Price")
-    ax.legend()
-
+    fig, ax = mpf.plot(tickerDf, type='candle', style='yahoo', returnfig=True)
     st.pyplot(fig)
+
+st.title('OHLC Chart')
+ticker_symbol = st.text_input('Enter a ticker symbol (e.g., AAPL, GOOGL)')
+
+if ticker_symbol:
+    plot_ohlc(ticker_symbol)
