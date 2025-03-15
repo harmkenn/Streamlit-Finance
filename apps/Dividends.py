b4637afdec7c9d4579169aa47b90a68a8eb2b460
Ken Harmon <harmkenn@gmail.com>
2025-03-15 09:20:31 -0600

    Cleaned up

--- apps/Dividends.py
+++ apps/Dividends.py
@@ -6,21 +6,24 @@ from datetime import datetime, timedelta
 st.title("Dividend Information App")
 
 st.header("Input Stock Ticker and Date Range")
+
 col1, col2, col3 = st.columns(3)
 with col1:
-    start_date = st.date_input("Start Date",
-                               datetime.today() - timedelta(days=2 * 365))
+    start_date = st.date_input("Start Date", value=datetime.today() - timedelta(days=2 * 365))
 with col2:
-    end_date = st.date_input("End Date", datetime.today())
+    end_date = st.date_input("End Date", value=datetime.today())
 with col3:
-    # Ticker input
-    ticker = st.text_input("Enter Stock Ticker", "MSTY").upper()
+    ticker = st.text_input("Enter Stock Ticker", value="MSTY").upper()
 
 stock = yf.Ticker(ticker)
 historical_data = stock.history(start=start_date, end=end_date, actions=True)
+
+# Extract relevant data
 dividends = historical_data['Dividends']
 historical_prices = historical_data['Close']
-div_data = historical_data[['Close', 'Dividends']]
+
+# Create dividend data frame
+div_data = historical_data[['Close', 'Dividends']].copy()
 div_data['Dividend Yield'] = (div_data['Dividends'] / div_data['Close']) * 100
 div_data = div_data[div_data['Dividends'] != 0]
 
