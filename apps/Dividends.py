5f7b30347e5e9e7ce58e5b098b000da54178be08
Ken Harmon <harmkenn@gmail.com>
2025-03-15 06:43:50 -0600

    Dividends

--- apps/Dividends.py
+++ apps/Dividends.py
@@ -5,10 +5,13 @@ import pandas as pd
 st.title("Dividend Information App")
 
 st.header("Input Stock Ticker and Date Range")
-
-ticker = st.text_input("Enter stock ticker:", value="AAPL")
-start_date = st.date_input("Enter start date:", value=pd.to_datetime("2020-01-01"))
-end_date = st.date_input("Enter end date:", value=pd.to_datetime("2022-02-26"))
+col1,col2,col3 = st.columns(3)
+with col1:
+    ticker = st.text_input("Enter stock ticker:", value="AAPL")
+with col2:
+    start_date = st.date_input("Enter start date:", value=pd.to_datetime("2024-01-01"))
+with col3:
+    end_date = st.date_input("Enter end date:", value=pd.to_datetime("today"))
 
 if st.button("Get Dividend Information"):
     stock_data = yf.Ticker(ticker)
