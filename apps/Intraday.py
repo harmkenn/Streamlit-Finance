23e916b63caafe6230988d64455d6108c44c0fd6
Ken Harmon <harmkenn@gmail.com>
2025-03-20 09:38:04 -0600

    Update 07

--- apps/Intraday.py
+++ apps/Intraday.py
@@ -9,7 +9,7 @@ stock_symbol = st.text_input("Enter stock symbol (e.g. AAPL, GOOG, MSFT):")
 
 if stock_symbol:
     ticker = yf.Ticker(stock_symbol)
-    data = ticker.history(start="2025-02-18", end="2022-01-21", interval="1m")
+    data = ticker.history(period="5d", interval="1m")
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
