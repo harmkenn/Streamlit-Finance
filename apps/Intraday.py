78e95adb5b5bb6369516d36034c10282d9ff09c5
Ken Harmon <harmkenn@gmail.com>
2025-03-20 09:47:29 -0600

    Update 10

--- apps/Intraday.py
+++ apps/Intraday.py
@@ -2,33 +2,28 @@ import streamlit as st
 import yfinance as yf
 import plotly.express as px
 from plotly.subplots import make_subplots
-import time
 
 st.title("Intraday Stock Prices")
 
 stock_symbol = st.text_input("Enter stock symbol (e.g. AAPL, GOOG, MSFT):")
 
 if stock_symbol:
-    while True:
-        ticker = yf.Ticker(stock_symbol)
-        data = ticker.history(period="5d", interval="1m")
+    ticker = yf.Ticker(stock_symbol)
+    data = ticker.history(period="5d", interval="1m")
 
-        # Reverse the order of the data to get the newest row first
-        data = data.iloc[::-1]
+    data = data.iloc[::-1]
 
-        fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-        fig.add_trace(px.line(data, x=data.index, y="Close").data[0], secondary_y=False)
+    fig.add_trace(px.line(data, x=data.index, y="Close").data[0], secondary_y=False)
 
-        fig.add_trace(px.bar(data, x=data.index, y="Volume").data[0], secondary_y=True)
+    fig.add_trace(px.bar(data, x=data.index, y="Volume").data[0], secondary_y=True)
 
-        fig.data[1].marker.color = 'red'
+    fig.data[1].marker.color = 'red'
 
-        fig.update_layout(title=f"{stock_symbol} Intraday Prices", xaxis_title="Time", yaxis_title="Price")
-        fig.update_yaxes(title_text="Volume", secondary_y=True)
+    fig.update_layout(title=f"{stock_symbol} Intraday Prices", xaxis_title="Time", yaxis_title="Price")
+    fig.update_yaxes(title_text="Volume", secondary_y=True)
 
-        st.plotly_chart(fig)
+    st.plotly_chart(fig)
 
-        st.write(data)
-
-        time.sleep(60)  # wait 1 minute before refreshing
+    st.write(data)
