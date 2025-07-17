c601ad98df0fab8a36d29de7960ed3c9561a6137
Ken Harmon <harmkenn@gmail.com>
2025-07-17 17:25:50 +0200

    Update cleaner

--- etrade_api/market.py
+++
@@ -1,9 +0,0 @@
-class Market:
-    def __init__(self, session, base_url):
-        self.session = session
-        self.base_url = base_url
-
-    def get_quote(self, symbol):
-        url = f"{self.base_url}/v1/market/quote/{symbol}.json"
-        r = self.session.get(url)
-        return r.json() if r.status_code == 200 else None
