8ae487f1a56dfeb8194a5dbf420de90470238f2a
harmkenn <harmkenn@gmail.com>
2025-10-30 08:35:05 +0000

    Update refresh

--- apps/holdvstrade.py
+++ apps/holdvstrade.py
@@ -16,6 +16,7 @@ refresh_button = st.button("🔄 Refresh Data")
 # --- Main Columns ---
 if tickers_list:
     if refresh_button:
+        # Rerun the app only when the refresh button is clicked
         st.experimental_rerun()
 
     for t in tickers_list:
