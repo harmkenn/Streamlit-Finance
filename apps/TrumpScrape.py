5369ab3012c2771c8ae50f92eb1e58c9b5e0e171
Ken Harmon <harmkenn@gmail.com>
2026-04-09 09:26:00 +0200

    Update TrumpScrape.py

--- apps/TrumpScrape.py
+++ apps/TrumpScrape.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
 import re
-
+#
 st.set_page_config(page_title="HTML → CSV Extractor", layout="wide")
 
 st.title("HTML → CSV Extractor for Truth Social Posts")
