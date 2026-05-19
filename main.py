bfcb37c086326713bebe425f1a19f1110d7aad5b
Ken Harmon <harmkenn@gmail.com>
2026-05-19 11:13:47 +0200

    Update to tickers

--- main.py
+++ main.py
@@ -41,8 +41,7 @@ if "tickers" not in st.session_state:
 
 tickers_list = st.sidebar.text_area(
     "Enter comma-separated stock tickers",
-    value=st.session_state["tickers"],
-    height=100
+    value=st.session_state.get("tickers", "TQQQ,UPRO,UDOW,BNO,NVTS,MXL,^VIX"),height=100
 )
 
 # Save edits when user changes
